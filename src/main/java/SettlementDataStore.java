db7daffdd78eea010589257a3cda8ff88aa934e0
Michel R Vaillancourt <michel.vaillancourt@thinkingbig.net>
2019-11-08 09:50:37 -0400

    Adding get/set logic for population related data

--- src/main/java/SettlementDataStore.java
+++ src/main/java/SettlementDataStore.java
@@ -1,17 +1,30 @@
 package com.pe902_gaming.FantasySettlementDataManager.model;
 
+import java.util.Arrays;
+import java.util.ArrayList;
+
 public class SettlementDataStore
 {
-  private int AveragePopulation;
-  private double final WealthyPopPercent = 0.02;
+  final private double PopSummerPercent = 0.09;
+  final private double PopWinterPercent = 1.10;
+
+  private ArrayList<String[]> PopEconomicClassPercent;
 
   private String NationName;
   private String SettlementName;
+  private int AveragePopulation;
 
   private SettlementDataStore(SettlementDataStoreBuilder builder)
   {
     AveragePopulation = builder.AveragePopulation;
     SettlementName = builder.SettlementName;
+
+    PopEconomicClassPercent.add( new String[]{ "Poor" , "0.52" } );
+    PopEconomicClassPercent.add( new String[]{ "Modest", "0.30" } );
+    PopEconomicClassPercent.add( new String[]{ "Young", "0.125" } );
+    PopEconomicClassPercent.add( new String[]{ "Comfortable", "0.03" } );
+    PopEconomicClassPercent.add( new String[]{ "Wealthy", "0.01" } );
+    PopEconomicClassPercent.add( new String[]{ "Aristocratic", "0.005" } );
   } // constructor SettlementDataStore
 
   public static class SettlementDataStoreBuilder
@@ -41,5 +54,27 @@ public class SettlementDataStore
     }
 
   } // builder pattern class
+
+  public int getAveragePopulation() { return AveragePopulation; }
+  public void setAveragePopulation( int averagePopulation ) { AveragePopulation = averagePopulation; }
+  public int getSummerPopulation() { return (int)(Math.round ( AveragePopulation * PopSummerPercent )); }
+  public int getWinterPopulation() { return (int)(Math.round ( AveragePopulation * PopWinterPercent )); }
+
+  public int getPopEconomicClassPercent( String ClassDescription )
+  {
+    double ClassPopPercent = 0.00;
+    for ( String[] DataStringArray : PopEconomicClassPercent )
+    {
+      if ( DataStringArray[0].toLowerCase() == ClassDescription.toLowerCase() )
+        ClassPopPercent = Double.parseDouble(DataStringArray[1]);
+    }
+    return (int)(Math.round ( AveragePopulation * ClassPopPercent ));
+  } // end getPopEconomicClassPercent
+
+  public String getNationName() { return NationName; }
+  public void setNationName( String nationName ) { NationName = nationName; }
+
+  public String getSettlementName() { return SettlementName; }
+  public void setSettlementName( String settlementName) { SettlementName = settlementName; }
 
 } // end class SettlementDataStore
